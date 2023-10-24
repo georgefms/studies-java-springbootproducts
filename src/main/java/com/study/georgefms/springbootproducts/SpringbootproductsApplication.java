b243b96068020f7cda0b822a5d97cb98c7751439
George Felipe de Magalhães Silva <georgefelipe8@gmail.com>
2023-10-24 10:15:37 -0300

    Initial Commit

---
+++ src/main/java/com/study/georgefms/springbootproducts/SpringbootproductsApplication.java
@@ -0,0 +1,13 @@
+package com.study.georgefms.springbootproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootproductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootproductsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/georgefms/springbootproducts/SpringbootproductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.georgefms.springbootproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootproductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
