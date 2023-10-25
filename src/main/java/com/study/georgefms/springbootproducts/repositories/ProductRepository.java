8be1f78cdff580fd94de0f703ee0c0ad26ab9fb3
George Felipe de Magalhães Silva <georgefelipe8@gmail.com>
2023-10-25 10:55:33 -0300

    Feat: create product repository

---
+++ src/main/java/com/study/georgefms/springbootproducts/repositories/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.study.georgefms.springbootproducts.repositories;
+
+import com.study.georgefms.springbootproducts.models.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+    
+}
