1040f53e35f72133c090599beb8dd325cdea95f2
George Felipe de Magalhães Silva <georgefelipe8@gmail.com>
2023-11-13 11:49:09 -0300

    Feat: Implementing register route

--- src/main/java/com/study/georgefms/springbootproducts/controller/AuthController.java
+++ src/main/java/com/study/georgefms/springbootproducts/controller/AuthController.java
@@ -1,11 +1,16 @@
 package com.study.georgefms.springbootproducts.controller;
 
 import com.study.georgefms.springbootproducts.dto.AuthDTO;
+import com.study.georgefms.springbootproducts.dto.RegisterDTO;
+import com.study.georgefms.springbootproducts.models.UserModel;
+import com.study.georgefms.springbootproducts.repositories.UserRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +22,8 @@ public class AuthController {
 
     @Autowired
     private AuthenticationManager authenticationManager;
+    @Autowired
+    private UserRepository userRepository;
 
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody @Valid AuthDTO authDTO){
@@ -26,5 +33,13 @@ public class AuthController {
         return ResponseEntity.ok().build();
     }
 
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody @Valid RegisterDTO registerDTO){
+        if(this.userRepository.findByLogin(registerDTO.login()) != null) return ResponseEntity.badRequest().build();
+        String encryptedPass = new BCryptPasswordEncoder().encode(registerDTO.password());
+        UserModel newUser = new UserModel(registerDTO.login(), encryptedPass, registerDTO.role());
 
+        this.userRepository.save(newUser);
+        return ResponseEntity.ok().build();
+    }
 }
